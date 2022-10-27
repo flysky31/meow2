86a8a7f34e50974da3edf4696edf0288b6e73a88
flysky31 <70322692+flysky31@users.noreply.github.com>
2022-10-27 18:17:08 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let $header = $("#header"),
+    $header_offset = $header.offset().top;
+
+$(window).scroll(function(){
+    if( $(window).scrollTop() > 50){
+        $header.addClass("on");
+    } else {
+        $header.removeClass("on");
+    }
+});
+    
+
+$(".gnb_m").click(function(){
+    $(".navbar").toggleClass("on");
+
+    if($(".navbar").hasClass("on")){
+        $("#header").addClass("bg_show");
+    } else {
+        $("#header").removeClass("bg_show");
+    }
+})
+
+
+$(".nav-item").click(function(){
+    $('.navbar').hide();
+    $('#header').removeClass("bg_show")
+})
+
+$(".top_btn").click(function(){
+    $("html,body").animate({
+        scrollTop : '0'
+    },100)
+
+   
+})
+
+
+
+var swiper = new Swiper(".sec02_slide", {
+    slidesPerView: 3,
+    autoplay: {
+        delay: 2000,
+    },
+    loop:true,
+    speed: 1000,
+    breakpoints: {        
+        767: {
+          slidesPerView: 2
+        },
+        480: {
+            slidesPerView: 1
+        },
+    }
+  });
